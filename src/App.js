ca1ab70d203a4223332cd9a4468cea05dc16a70d
Antonin Jares <jares.antonin@gmail.com>
2020-11-27 14:45:55 +0100

    Use hooks

--- src/App.js
+++ src/App.js
@@ -1,64 +1,145 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Layout, Affix, message } from 'antd';
 import {CurrencyList} from './components/currency-list';
 import {PageHeader, PageTitle, PageBody, PageLogoContainer, SearchBar} from './App.styled';
 import { filterData, parseFxData } from './components/currency-list/fx-data';
 import { getFxData } from './api/fx-data';
 
-class App extends React.Component {
-  state = {filterText: '', loading: true, filteredRows: []};
+const registerOnHashChange = onHashChange => {
+  window.onhashchange = onHashChange;
+}
 
-  componentDidMount() {
-    const url = new URL(window.location);
-    const filterText = url.hash.slice(1);
-    this.setState({filterText}, this.refreshData);
-  }
+const App = () => {
+  const [filteredRows, setFilteredRows] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [filterText, setFilterText] = useState('');
+  const rows = useRef([]);
 
-  refreshData = () =>
+  const refreshData = () => {
     getFxData()
       .then(parseFxData)
-      .then(({rows}) => {
-        this.rows = rows;
-        this.updateData();
+      .then(({rows: data}) => {
+        rows.current = data;
+        updateData();
       })
       .catch(e => {
         console.error(e);
         message.error('There was an error downloading the data, please try again later.')
       })
-      .finally(() => this.setState({loading: false}))
-
-  updateFilterText = e => {
-    const filterText = e.target.value;
-    window.location.hash = filterText;
-    this.setState({filterText}, this.updateData);
+      .finally(() => setLoading(false))
   };
 
-  updateData = () => {
-    const {filterText} = this.state;
-
+  const updateData = filterText => {
     // Prevent input field typing lag
-    setTimeout(() => this.setState({filteredRows: filterData(this.rows, filterText)}));
+    setTimeout(() => setFilteredRows(filterData(rows.current, filterText)));
   }
 
-  render() {
-    const {loading, filteredRows} = this.state;
-    return (
-      <Layout>
-        <PageHeader>
-          <PageLogoContainer>
-            <img src="/logo.svg" />
-          </PageLogoContainer>
-          <PageTitle>George FX Test</PageTitle>
-        </PageHeader>
-        <PageBody>
-          <Affix>
-            <SearchBar onChange={this.updateFilterText} value={this.state.filterText}/>
-          </Affix>
-          <CurrencyList data={filteredRows} loading={loading}/>
-        </PageBody>
-      </Layout>
-    );
-  }
-}
+  const onHashChange = e => {
+    const newUrl = new URL(e.newURL);
+    const filterText = newUrl.hash.slice(1);
+    setFilterText(filterText)
+    updateData(filterText);
+  };
+
+  useEffect(() => {
+    const url = new URL(window.location);
+    const filterText = url.hash.slice(1);
+    setFilterText(filterText)
+    refreshData();
+    registerOnHashChange(onHashChange);
+  }, []);
+
+  const updateFilterText = e => {
+    const filterText = e.target.value;
+    window.location.hash = filterText;
+    setFilterText(filterText);
+    updateData(filterText);
+  };
+
+  return (
+    <Layout>
+      <PageHeader>
+        <PageLogoContainer>
+          <img src="/logo.svg" />
+        </PageLogoContainer>
+        <PageTitle>George FX Test</PageTitle>
+      </PageHeader>
+      <PageBody>
+        <Affix>
+          <SearchBar onChange={updateFilterText} value={filterText}/>
+        </Affix>
+        <CurrencyList data={filteredRows} loading={loading}/>
+      </PageBody>
+    </Layout>
+  );
+};
+
+
+// class App extends React.Component {
+//   state = {filterText: '', loading: true, filteredRows: []};
+//
+//   // componentDidMount() {
+//   //   const url = new URL(window.location);
+//   //   const filterText = url.hash.slice(1);
+//   //   this.setState({filterText}, this.refreshData);
+//   //   this.registerOnHashChange();
+//   // }
+//
+//   onHashChange = e => {
+//     const newUrl = new URL(e.newURL);
+//     const filterText = newUrl.hash.slice(1);
+//     this.setState({filterText}, this.updateData);
+//   };
+//
+//   registerOnHashChange() {
+//     window.onhashchange = this.onHashChange;
+//   }
+//
+//   // refreshData = () =>
+//   //   getFxData()
+//   //     .then(parseFxData)
+//   //     .then(({rows}) => {
+//   //       this.rows = rows;
+//   //       this.updateData();
+//   //     })
+//   //     .catch(e => {
+//   //       console.error(e);
+//   //       message.error('There was an error downloading the data, please try again later.')
+//   //     })
+//   //     .finally(() => this.setState({loading: false}))
+//
+//   // updateFilterText = e => {
+//   //   const filterText = e.target.value;
+//   //   window.location.hash = filterText;
+//   //   this.setState({filterText}, this.updateData);
+//   // };
+//
+//   // updateData = () => {
+//   //   const {filterText} = this.state;
+//   //
+//   //   // Prevent input field typing lag
+//   //   setTimeout(() => this.setState({filteredRows: filterData(this.rows, filterText)}));
+//   // }
+//
+//   // render() {
+//   //   const {loading, filteredRows} = this.state;
+//   //   return (
+//   //     <Layout>
+//   //       <PageHeader>
+//   //         <PageLogoContainer>
+//   //           <img src="/logo.svg" />
+//   //         </PageLogoContainer>
+//   //         <PageTitle>George FX Test</PageTitle>
+//   //       </PageHeader>
+//   //       <PageBody>
+//   //         <Affix>
+//   //           <SearchBar onChange={this.updateFilterText} value={this.state.filterText}/>
+//   //         </Affix>
+//   //         <CurrencyList data={filteredRows} loading={loading}/>
+//   //       </PageBody>
+//   //     </Layout>
+//   //   );
+//   // }
+// }
 
 export default App;
