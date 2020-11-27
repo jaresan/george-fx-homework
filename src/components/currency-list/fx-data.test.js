7a701b797160cdad629b5b87d4efe06f9a01161e
Antonin Jares <jares.antonin@gmail.com>
2020-11-27 15:15:59 +0100

    Add tests

--- src/components/currency-list/fx-data.test.js
+++ src/components/currency-list/fx-data.test.js
@@ -21,60 +21,76 @@ const data = {
   ]
 };
 
-describe('fx data parsing', () => {
-  it('should parse fx data', () => {
-    expect(parseFxData(data)).toEqual({
-      baseCurrency: "EUR",
-      rows: [
-        {
-          info: {
-            code: "FJ",
-            currency: "FJD",
-            flagPath: "flags/fj.png",
-            name: "Fiji"
+describe('fx data handling', () => {
+  describe('#parseFxData()', () => {
+    it('should parse fx data', () => {
+      expect(parseFxData(data)).toEqual({
+        baseCurrency: "EUR",
+        rows: [
+          {
+            info: {
+              code: "FJ",
+              currency: "FJD",
+              flagPath: "flags/fj.png",
+              name: "Fiji"
+            },
+            key: "FJ",
+            rates: {
+              buy: 2,
+              middle: 2.25,
+              sell: 2.5
+            },
+            searchTerm: "fj fiji fjd"
           },
-          key: "FJ",
-          rates: {
-            buy: 2,
-            middle: 2.25,
-            sell: 2.5
-          },
-          searchTerm: "fj fiji fjd"
-        },
-        {
-          info: {
-            code: "MX",
-            currency: "MXN",
-            flagPath: "flags/mx.png",
-            name: "Mexico"
-          },
-          key: "MX",
-          rates: {
-            buy: 22.38,
-            middle: 22.98,
-            sell: 23.58
-          },
-          searchTerm: "mx mexico mxn"
-        }
-      ]
+          {
+            info: {
+              code: "MX",
+              currency: "MXN",
+              flagPath: "flags/mx.png",
+              name: "Mexico"
+            },
+            key: "MX",
+            rates: {
+              buy: 22.38,
+              middle: 22.98,
+              sell: 23.58
+            },
+            searchTerm: "mx mexico mxn"
+          }
+        ]
+      });
+    })
+  })
+
+  describe('#filterData()', () => {
+    it('should return no rows for no matching search terms', () => {
+      const rows = [{
+        key: "FJ",
+        searchTerm: "fj fiji fjd"
+      }];
+      expect(filterData(rows, 'country')).toEqual([]);
+    });
+
+    it('should return rows matching search terms', () => {
+      const rows = [{
+        key: "FJ",
+        searchTerm: "fj fiji fjd"
+      }];
+
+      const res = filterData(rows, 'fj');
+
+      expect(res).toEqual([{key: "FJ", searchTerm: "fj fiji fjd"}]);
+    });
+
+    it('should all rows for empty search string', () => {
+      const rows = [{
+        key: "FJ",
+        searchTerm: "fj fiji fjd"
+      }];
+
+      const res = filterData(rows, '');
+
+      expect(res).toEqual([{key: "FJ", searchTerm: "fj fiji fjd"}]);
     });
   })
-  it('should filter data by search term', () => {
-    expect(filterData(parseFxData(data).rows, 'mex')).toEqual([{
-      info: {
-        code: "MX",
-        currency: "MXN",
-        flagPath: "flags/mx.png",
-        name: "Mexico"
-      },
-      key: "MX",
-      rates: {
-        buy: 22.38,
-        middle: 22.98,
-        sell: 23.58
-      },
-      searchTerm: "mx mexico mxn"
-    }])
-    expect(filterData(parseFxData(data).rows, 'randomtext')).toEqual([])
-  })
-});
+})
